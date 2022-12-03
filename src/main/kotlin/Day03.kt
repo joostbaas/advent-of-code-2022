2eec1379bc687a47535e5b24b81ccd55005a971b
Joost Baas <joost@joostbaas.eu>
2022-12-03 18:36:53 +0100

    simplify

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -4,22 +4,19 @@ fun Char.priorityValue(): Int = when (this) {
     else -> throw IllegalArgumentException()
 }
 
-fun charsInCommon(vararg s: String): Set<Char> =
-    s.toList().fold(s[0].toSet()) { acc, next ->
+fun charsInCommon(s: List<String>): Set<Char> =
+    s.fold(s[0].toSet()) { acc, next ->
         acc intersect next.toSet()
     }
 
 fun <T> Set<T>.onlyElement(): T = if (size != 1) throw IllegalArgumentException() else first()
 
 fun day03Part1(input: List<String>): Int = input.sumOf { line ->
-    val (firstHalf, secondHalf) = line.chunked(line.length / 2)
-    val charThatAppearsInBothHalves = charsInCommon(firstHalf,  secondHalf)
-    charThatAppearsInBothHalves.onlyElement().priorityValue()
+    val halfStrings = line.chunked(line.length / 2)
+    charsInCommon(halfStrings).onlyElement().priorityValue()
 }
 
 fun day03Part2(input: List<String>): Int = input.chunked(3)
     .sumOf { group: List<String> ->
-        val (chunk0, chunk1, chunk2) = group
-        val charsThatAppearsInAllThree = charsInCommon(chunk0, chunk1, chunk2)
-        charsThatAppearsInAllThree.onlyElement().priorityValue()
+        charsInCommon(group).onlyElement().priorityValue()
     }
