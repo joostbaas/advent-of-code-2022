555767fb831092557965550f27967d884a3fd117
Joost Baas <joost@joostbaas.eu>
2023-12-06 21:58:58 +0200

    remove unused md5

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,6 +1,3 @@
-import java.math.BigInteger
-import java.security.MessageDigest
-
 /**
  * Reads lines from the given input txt file.
  */
@@ -8,9 +5,3 @@ fun readInput(name: String) = object {}.javaClass.classLoader.getResource("$name
     ?.readText()
     ?.lines()!!
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
